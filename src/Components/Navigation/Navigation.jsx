faf8f58208f71142f87a4a7f4ec899abbf4aa236
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-11-03 13:39:05 +0200

    deleted external file

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import "./Navigation.css";
-import "./navigationData";
-
-// Importing navigation links to render them dinamically
-import { navigationLinks } from "./navigationData";
 
 // Simple Navigation component with a logo and links.
 const Navigation = () => {
+  const navigationLinks = ["Home", "Recipes", "AboutMe"];
+
   return (
     <nav className="app_navigation">
       <h1 className="app_navigation_logo">Green bite</h1>
       <ul className="app_navigation_links">
-        {navigationLinks.map(({ id, label, href }) => (
-          <li key={id}>
-            <a
-              href={href}
-              className={activePage === id ? "active" : ""}
-              aria-current={activePage === id ? "page" : undefined}
-            >
-              {label}
-            </a>
+        {navigationLinks.map((link, index) => (
+          <li key={index}>
+            <a href={link}>{link}</a>
           </li>
         ))}
       </ul>
--- src/Components/Navigation/navigationData.js
+++
@@ -1,6 +0,0 @@
-// Exporting navigation links
-export const navigationLinks = [
-  { id: "home", label: "Home", href: "#home" },
-  { id: "recipes", label: "Recipes", href: "#recipes" },
-  { id: "aboutMe", label: "About Me", href: "#aboutMe" },
-];
