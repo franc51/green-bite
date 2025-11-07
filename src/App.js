aa0d918284e1b13232298d6d9330603bf1a6253a
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2025-11-07 13:22:03 +0200

    started form functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Navigation from "./Components/Navigation/Navigation";
-import RecipeCard from "./Components/RecipeCard/RecipeCard";
+import RecipeForm from "./Components/RecipeForm/RecipeForm";
 
 // Main function of the application that renders everything
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-      <RecipeCard></RecipeCard>
+      <RecipeForm></RecipeForm>
     </div>
   );
 }
